d22ca09b2d2351c12d6438b06e13bf11997874ce
Dawit <dawitmekuria5@gmail.com>
2022-06-10 18:01:08 -0400

    removed discord.io client

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
-
 const { Client, Collection, Intents } = require('discord.js');
-
-const Discord = require("discord.io");
 const { token } = require('./config.json');
+const { API_KEY } = require('./apex-api.json');
 
-const bot = new Discord.Client({
-	token: token,
-
-	autorun: true,
-})
 // Create a new client instance Discord.js
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-// Discord.io
-bot.on("ready", function(evt) {
-	console.log('Ready from Discord.io.');
-});
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
 
 function calcTime(time) {
 	var hours = Math.floor(time/3600%60);
@@ -26,57 +14,6 @@ function calcTime(time) {
 	var seconds = Math.floor(time%60);
 	return hours +'h: ' + minutes +'m: ' + seconds+'s';
 }
-
-bot.on("message", function (user, userID, channelID, message, evt) {
-	// Our bot needs to know if it will execute a command
-  
-	// It will listen for messages that will start with `!`
-  
-	if (message.substring(0, 2) == "a!") {
-	  var args = message.substring(2).split(" ");
-	  console.log(args);
-  
-	  var cmd = args[0];
-  
-	  switch (cmd) {
-		  case "map":
-			  const API_KEY = "eada3f695e72a1c2b6e07229190c846a";
-			  var XMLHttpRequest = require('xhr2');
-			  var http = new XMLHttpRequest();
-  
-			  http.onreadystatechange = function () {
-				  if(this.readyState == 4 && this.status == 200) {
-					  var obj = JSON.parse(http.responseText);
-					  console.log(args);
-					  if(args[1] == 'ar') {
-						if(args[2] === 'ranked') {
-						  bot.sendMessage({
-							to: channelID,
-							message: `Current Ranked Arenas Map: ${obj.arenasRanked.current.map}\nTime Left: ${calcTime(obj.arenasRanked.current.remainingSecs)}\n\nNext Map: ${obj.arenasRanked.next.map}
-							`,
-						  });
-						} else {
-						  bot.sendMessage({
-							to: channelID,
-							message: `Current Arenas Map: \n${obj.arenas.current.map}\nTime Left: ${calcTime(obj.arenas.current.remainingSecs)}\n\nNext Map: ${obj.arenas.next.map}
-							`,
-						  });
-						}
-					  } else {
-						bot.sendMessage({
-							to: channelID,
-							message: `Current Map: ${obj.battle_royale.current.map}\nTime Left: ${calcTime(obj.battle_royale.current.remainingSecs)}\n\nNext Map: ${obj.battle_royale.next.map}
-							`,
-						});
-					  }
-				  }
-			  }
-			  http.open("GET", `https://api.mozambiquehe.re/maprotation?auth=${API_KEY}&version=2`, true);
-			  http.send();
-			  break;
-	  }
-	}
-  });
 
 client.commands = new Collection();
 
@@ -98,6 +35,49 @@ client.once('ready', () => {
 	console.log('Ready from discord.js!');
 });
 
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return false; 
+  
+  if (message.content.substring(0, 2) == "a!") {
+	var args = message.content.substring(2).split(" ");
+	console.log(args);
+
+	var cmd = args[0];
+
+	switch (cmd) {
+		case "map":
+			var XMLHttpRequest = require('xhr2');
+			var http = new XMLHttpRequest();
+
+			http.onreadystatechange = function () {
+				if(this.readyState == 4 && this.status == 200) {
+					var obj = JSON.parse(http.responseText);
+					console.log(args);
+					if(args[1] == 'ar') {
+
+					  if(args[2] === 'ranked') {
+						message.channel.send(
+							`Current Ranked Arenas Map: ${obj.arenasRanked.current.map}\nTime Left: ${calcTime(obj.arenasRanked.current.remainingSecs)}\n\nNext Map: ${obj.arenasRanked.next.map}
+						  `,
+						);
+					  } else {
+						message.channel.send(
+							`Current Arenas Map: ${obj.arenas.current.map}\nTime Left: ${calcTime(obj.arenas.current.remainingSecs)}\n\nNext Map: ${obj.arenas.next.map}
+						  `,);
+					  }
+					} else {
+						message.channel.send(
+							`Current Map: ${obj.battle_royale.current.map}\nTime Left: ${calcTime(obj.battle_royale.current.remainingSecs)}\n\nNext Map: ${obj.battle_royale.next.map}
+						  `,);
+					}
+				}
+			}
+			http.open("GET", `https://api.mozambiquehe.re/maprotation?auth=${API_KEY}&version=2`, true);
+			http.send();
+			break;
+	}
+  }
+});
 //	Discord.js
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
